05ecf110b6167dc49ab449f35011104243c45a9e
Christian Moulton <christianandrew.moulton@gmail.com>
2020-01-05 22:56:40 -0600

    probably this is out of scope to work

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,29 +1,32 @@
-let rps = prompt("Pick Rock or Paper or Scissors", "");
+function rps = prompt("Pick Rock or Paper or Scissors", "");
+function rps1 = rps.toLowerCase();
 
-let rps2 = Math.floor(Math.random() * 3) + 1;
+function rps2 = Math.floor(Math.random() * 3) + 1;
 
-if (rps = "Rock", "rock", "ROCK") {
-    let rps = 1;
-} else if (rps = "Paper", "paper", "PAPER") {
-    let rps = 2;
-} else if (rps = "Scissors", "scissors", "SCISSORS") {
-    let rps = 3;
-} else let result = "Invalid Selection";    
+function result()
 
-if (rps = 1, rps2 = 2) {
+if (rps1 = "rock") {
+    let rps3 = (1);
+} else if (rps1 = "paper") {
+    let rps3 = (2);
+} else if (rps1 = "scissors") {
+    let rps3 = (3);
+} else let rps3 = (4);    
+
+if (rps3 = 1 && rps2 = 2) {
     let result = ("I chose paper. You lose!");
-} else if (rps = 2, rps2 = 3) {
+} else if (rps3 = 2 && rps2 = 3) {
     let result = ("I chose scissors. You lose!");
-} else if (rps = 3, rps2 = 1) {
+} else if (rps3 = 3 && rps2 = 1) {
     let result = ("I chose rock. You lose!");
-} else if (rps = 2, rps2 = 1) {
+} else if (rps3 = 2 && rps2 = 1) {
     let result = ("I chose rock. You win!");
-} else if (rps = 3, rps2 = 2) {
+} else if (rps3 = 3 && rps2 = 2) {
     let result = ("I chose paper. You win!");
-} else if (rps = 1, rps2 = 3) {
+} else if (rps3 = 1 && rps2 = 3) {
     let result = ("I chose scissors. You win!");
-} else if (rps = rps2) {
+} else if (rps3 = rps2) {
     let result = ("I chose the same. Tie!");
-}
+} else let result = ("Invalid answer");
 
 alert(result);
