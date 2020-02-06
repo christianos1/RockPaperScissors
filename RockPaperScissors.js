5b320bc034f2b3c459dbbf64377fb48c8b65948f
Christian Moulton <christianandrew.moulton@gmail.com>
2020-02-05 21:20:26 -0600

    I think I've got it!

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,13 +1,13 @@
+var pscore = 0;
+var cscore = 0;
+
 var choose = prompt("Pick rock, paper or scissors", "");
 console.log(choose);
 choose = choose.toLowerCase();
-
 var computer = Math.round(Math.random() * 3);
 console.log(computer);
 // 0=rock; 1=paper; 2=scissors
-
 var declare;
-
 if (computer == 0) {
     declare = "rock";
 }
@@ -16,16 +16,133 @@ else if (computer == 1) {
 }
 else declare = "scissors";
 console.log(declare);
+document.write("I choose " + declare + " .");
+if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer >= 2)) {
+document.write("Tie! <br/>");
+}
+else if ((choose == "rock" && computer >= 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
+document.write("You win! <br/>");
+pscore=pscore + 1;
+}
+else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer >= 2) || (choose == "scissors" && computer == 0)) {
+document.write("You lose! <br/>");
+cscore=cscore + 1;
+}
+document.write("player " + pscore + "; computer " + cscore);
 
+var choose = prompt("Pick rock, paper or scissors", "");
+console.log(choose);
+choose = choose.toLowerCase();
+var computer = Math.round(Math.random() * 3);
+console.log(computer);
+// 0=rock; 1=paper; 2=scissors
+var declare;
+if (computer == 0) {
+    declare = "rock";
+}
+else if (computer == 1) {
+    declare = "paper";
+}
+else declare = "scissors";
+console.log(declare);
+document.write("I choose " + declare + " .");
+if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer >= 2)) {
+document.write("Tie! <br/>");
+}
+else if ((choose == "rock" && computer >= 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
+document.write("You win! <br/>");
+pscore=pscore + 1;
+}
+else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer >= 2) || (choose == "scissors" && computer == 0)) {
+document.write("You lose! <br/>");
+cscore=cscore + 1;
+}
+document.write("player " + pscore + "; computer " + cscore);
+
+var choose = prompt("Pick rock, paper or scissors", "");
+console.log(choose);
+choose = choose.toLowerCase();
+var computer = Math.round(Math.random() * 3);
+console.log(computer);
+// 0=rock; 1=paper; 2=scissors
+var declare;
+if (computer == 0) {
+    declare = "rock";
+}
+else if (computer == 1) {
+    declare = "paper";
+}
+else declare = "scissors";
+console.log(declare);
+document.write("I choose " + declare + " .");
+if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer >= 2)) {
+document.write("Tie! <br/>");
+}
+else if ((choose == "rock" && computer >= 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
+document.write("You win! <br/>");
+pscore=pscore + 1;
+}
+else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer >= 2) || (choose == "scissors" && computer == 0)) {
+document.write("You lose! <br/>");
+cscore=cscore + 1;
+}
+document.write("player " + pscore + "; computer " + cscore);
+
+var choose = prompt("Pick rock, paper or scissors", "");
+console.log(choose);
+choose = choose.toLowerCase();
+var computer = Math.round(Math.random() * 3);
+console.log(computer);
+// 0=rock; 1=paper; 2=scissors
+var declare;
+if (computer == 0) {
+    declare = "rock";
+}
+else if (computer == 1) {
+    declare = "paper";
+}
+else declare = "scissors";
+console.log(declare);
+document.write("I choose " + declare + " .");
+if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer >= 2)) {
+document.write("Tie! <br/>");
+}
+else if ((choose == "rock" && computer >= 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
+document.write("You win! <br/>");
+pscore=pscore + 1;
+}
+else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer >= 2) || (choose == "scissors" && computer == 0)) {
+document.write("You lose! <br/>");
+cscore=cscore + 1;
+}
+document.write("player " + pscore + "; computer " + cscore);
+
+var choose = prompt("Pick rock, paper or scissors", "");
+console.log(choose);
+choose = choose.toLowerCase();
+var computer = Math.round(Math.random() * 3);
+console.log(computer);
+// 0=rock; 1=paper; 2=scissors
+var declare;
+if (computer == 0) {
+    declare = "rock";
+}
+else if (computer == 1) {
+    declare = "paper";
+}
+else declare = "scissors";
+console.log(declare);
 document.write("I choose " + declare + " .");
 
-
-if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer == 2)) {
-document.write("Tie!");
+if ((choose == "rock" && computer == 0) || (choose == "paper" && computer == 1) || (choose == "scissors" && computer >= 2)) {
+document.write("Tie! <br/>");
 }
-else if ((choose == "rock" && computer == 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
-document.write("You win!");
+else if ((choose == "rock" && computer >= 2) || (choose == "paper" && computer == 0) || (choose == "scissors" && computer == 1)) {
+document.write("You win! <br/>");
+pscore=pscore + 1;
 }
-else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer == 2) || (choose == "scissors" && computer == 0)) {
-document.write("You lose!");
+else if ((choose == "rock" && computer == 1) || (choose == "paper" && computer >= 2) || (choose == "scissors" && computer == 0)) {
+document.write("You lose! <br/>");
+cscore=cscore + 1;
 }
+document.write("player " + pscore + "; computer " + cscore);
